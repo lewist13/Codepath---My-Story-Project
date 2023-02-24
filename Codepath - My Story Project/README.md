1c45685c639f01f2b04b865b1a5ad0f0d1283c03
lewist13 <lewistpro@gmail.com>
2023-02-24 12:34:03 -0500

    finished project

---
+++ Codepath - My Story Project/README.md
@@ -0,0 +1,50 @@
+# Project 1 - *Lord Shadow's Story*
+
+Submitted by: **TaeVon Lewis**
+
+**Lord Shadow's Story** is an app that introduces Cid Kagenou and his squad Shadow Garden from the anime Eminence in the Shadow.
+
+Time spent: **About 1** hour spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] Users are able to see a customized app icon and launch screen
+- [x] Your app should work for any screen size and orientation using AutoLayout constraints
+  - [ ] Mark here if you will demostrate auto layout implemention as horizontal rotation or different phone size or both, then erase this text
+- [x] Users are able to see at least 3 context items
+- [x] Users are able to tap into each context item and see detail view contains information about each item (title, description, picture if applicable, etc.)
+ 
+The following **optional** features are implemented:
+
+- [ ] Improve and customize the user interface through styling and coloring
+- [ ] Allow users to scroll when context items are off-screen (table view implemention)
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src=https://imgur.com/7NPjUuz.gif title='Video Walkthrough' width=500 alt='Video Walkthrough' />
+
+GIF created with Gifski
+
+## Notes
+
+Storyboard isn't very intuitive but I didn't really have any issues completing the project.
+
+## License
+
+    Copyright [2023] [TaeVon Lewis]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- Codepath - My Story Project/ViewController.swift
+++ Codepath - My Story Project/ViewController.swift
@@ -2,18 +2,50 @@
 //  ViewController.swift
 //  Codepath - My Story Project
 //
-//  Created by TizzyMatic on 2/23/23.
+//  Created by TaeVon Lewis on 2/23/23.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    let contextTitles = ["About Me", "Powers and Abilities", "Shadow Garden Members"]
+        let contextImages = ["cid_kagenou", "cid_kagenou_powers", "shadow_garden"]
+        let contextDescriptions = [
+            "Fascinated by shadowbrokers, a young Japanese boy once trained for years to become a secret powerful individual while playing the role of a mob character in public. Having realized his limits as a human, he eventually came to the conclusion that he needed to attain magic. In his delusion, he ran into a truck and died, only to be reincarnated as Cid Kagenou, the second child of a noble family.",
+            "Iris Midgar, upon facing him under his \"Shadow\" persona, claimed Cid to be \"the most powerful being in the world\", even though he was not even fighting her seriously. In fact, this also applies to other worlds, defeating Ragnarok, a demon that ruled another dimension, with relative ease. So far, even though he has yet to go all-out, all of Cid's battles have ended with overwhelming victories, with the only opponents he had to get a \"little serious\" against being Aurora's projection, Beatrix, Elisabeth, Alpha, and the aforementioned Ragnarok.\nPowers include:\nPretty much everything you can imagine...",
+            "Shadow Garden is an mysterious hidden organization founded by Cid Kagenō also known as Shadow. Its primary goal was to eliminate the Cult of Diablos and prevent them from resurrecting the ancient demon, Diabolos."
+        ]
+    
+    var context: [Context] = []
+    
+    @IBOutlet var lordshadowpfp: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 0..<contextTitles.count {
+                    let newContext = Context(title: contextTitles[i], image: UIImage(named: contextImages[i])!, description: contextDescriptions[i])
+                    context.append(newContext)
+                }
+        
+        lordshadowpfp.layer.cornerRadius = lordshadowpfp.frame.size.width / 2
+        lordshadowpfp.clipsToBounds = true
     }
+    
+    @IBAction func didTapContext(_ sender: UIButton) {
+            performSegue(withIdentifier: "detailSegue", sender: sender)
+        }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
-
+        guard segue.identifier == "detailSegue",
+                  let tappedContext = (sender as? UIButton)?.tag,
+                  let detailViewController = segue.destination as? DetailViewController,
+                  tappedContext < context.count else {
+                print("Invalid context item selected.")
+                return
+            }
+            
+            detailViewController.context = context[tappedContext]
+    }
 }
-
