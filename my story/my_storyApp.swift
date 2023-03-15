f07d56e930eef02237fdd7e30ce76a227624d884
lewist13 <lewistpro@gmail.com>
2023-03-15 11:45:15 -0400

    Initial Commit

---
+++ my story/my_storyApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_storyApp.swift
+//  my story
+//
+//  Created by TizzyMatic on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct my_storyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
